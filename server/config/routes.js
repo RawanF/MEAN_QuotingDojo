9012e8ecc1991d25e38a08d0f8dd654c8dc78b69
RawanF <rawan.f.h@gmail.com>
2019-10-14 19:52:36 +0300

    cleaning code

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,7 +1,4 @@
-const mongoose = require('mongoose');
-const Quote = mongoose.model('Quote');
 const quotes = require('../controller/quotes.js');
-
 module.exports= function (app){
     app.get('/quotes', function (req, res) { 
         quotes.index(req, res);
