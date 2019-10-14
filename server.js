7c80710907d3a87c621baa6fce63d05c67fb5ca9
RawanF <rawan.f.h@gmail.com>
2019-10-14 19:48:46 +0300

    Modularizing: adding client folder and mongoose.js file

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const mongoose = require('mongoose');
 const app = express();
-app.use(express.static(__dirname + "/static"));
-app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + "/client/static"));
+app.set('views', __dirname + '/client/views');
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
-mongoose.connect('mongodb://localhost/QuotingDojo', { useNewUrlParser: true });
+require('./server/config/mongoose.js')
 require('./server/models/quote.js');
 require('./server/config/routes.js')(app)
 app.listen(8000, () => console.log("listening on port 8000")); 
---
+++ server/config/mongoose.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/QuotingDojo', { useNewUrlParser: true });
+// var models_path = path.join(__dirname, './../models');
+// fs.readdirSync(models_path).forEach(function(file) {
+//   if(file.indexOf('.js') >= 0) {
+//     require(models_path + '/' + file);
+//    }
+// })
